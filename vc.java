b43960e6b69b5effb2e2c3de29ec2bc299c9c619
preetha-ksr <preethamurugan91097@gmail.com>
2017-06-05 22:27:47 -0700

    Create vc.java

---
+++ vc.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.net.*;
+import java.util.*;
+class vc
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+char c=sc.next().charAt(0);
+if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u')
+{
+System.out.println("vowels");
+}
+else
+{
+System.out.println("consonant");
+}
+}
+}
+
+
