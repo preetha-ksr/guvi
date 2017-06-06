1e19f0affc3be56a75edfb33934bb056f22ff36f
preetha-ksr <preethamurugan91097@gmail.com>
2017-06-05 23:23:20 -0700

    Create hello.java

---
+++ hello.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+class hello
+{
+public static void main(String args[])
+{
+Scanner sc=new Scanner(System.in);
+ String s=sc.next();
+for(int i=0;i<5;i++)
+{
+System.out.println(s);
+}
+}
+}
